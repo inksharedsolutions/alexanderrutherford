14eb8252c2ab4769b9fbe59aa5d9763d3bd1b24f
inksharedsolutions <webdev.inkshared@gmail.com>
2020-09-24 02:49:48 +0800

    changes-9-24-2020

--- src/components/trailer.js
+++ src/components/trailer.js
@@ -11,16 +11,19 @@ const Trailer = () =>{
                     
                     <div className="grid-child-content">
                         <h5>Alexander Rutherford</h5>
+
                         <h1>
                             <span>Book</span>
                             <span>Trailer</span>
                         </h1>
+
                         <p>
                            Just like how the story of the book goes, 
                            the process in writing it was also a journey.
                            Hear about it from the author himself.
                            Click the play button and learn more.
                         </p>
+                        
                     </div>
 
                     <div className="grid-video-container">
