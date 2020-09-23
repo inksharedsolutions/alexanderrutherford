5458a692ddfb10a0c16abbdf5d5f778378764c9c
inksharedsolutions <webdev.inkshared@gmail.com>
2020-09-24 02:02:18 +0800

    changes-9-24-2020

--- src/pages/events.js
+++ src/pages/events.js
@@ -8,30 +8,123 @@ const Event = (props) =>{
 	return(
 		<>
 			<Layout>
-			<Nav pathExt={props.path}/>
+				<Nav pathExt={props.path}/>
 
-            <Banner 
-                spanFirst={``} 
-                contextHeading={`Events`}/>
+	            <Banner 
+	                spanFirst={``} 
+	                contextHeading={`Events`}/>
 
-			 	<h1>Events</h1> 
+	             	<div className="section-body-events">
+	             		<div className="container">
+					 		<h1 className="events-heading-section">Events</h1>
 
-			 	<section className="section-events">
-			 		<div className="events-info-body">
-			 			<h1>Salt City Comic Con</h1>
+						 	<section className="section-events">
+						 		<div className="events-info-body">
 
-			 			<div className="events-wrapper">
-				 			<span>Date</span>
-				 			<p>Saturday & Sunday, July 6 & 7, 2019</p>
-				 			<span>Place</span>
-				 			<p>Embassy Suites 311 Hiawatha Boulevard</p>
-				 			<p>W Syracause, New York 13204</p>
-				 			<span>Time</span>
-				 			<p>*Events subject to change</p>
-			 			</div>
+						 			<h1>Salt City Comic Con</h1>
 
-			 		</div>
-			 	</section>
+						 			<div className="events-wrapper">
+
+							 			<span className="events-title">Date</span>
+							 			<p>Saturday & Sunday, July 6 & 7, 2019</p>
+
+							 			<span className="events-title">Place</span>
+							 			<p>Embassy Suites 311 Hiawatha Boulevard</p>
+							 			<p>W Syracause, New York 13204</p>
+
+							 			<span className="events-title">Time</span>
+							 			<p>*Events subject to change</p>
+
+						 			</div>
+						 		</div>
+
+						 		<div className="events-info-body">
+						 			<h1>2019 Harlem Book Fair</h1>
+
+						 			<div className="events-wrapper">
+							 			
+							 			<span className="events-title">Date</span>
+							 			<p>Saturday July 20, 2019</p>
+							 			
+							 			<span className="events-title">Place</span>
+							 			<p>West 135 Street & Malcolm X Boulevard</p>
+
+							 			<span className="events-title">Time</span>
+							 			<p>9:00am to 6:00pm (expected, not verified)</p>
+							 			<p>*Event subject to change</p>
+
+						 			</div>
+						 		</div>
+					 		</section>
+
+					 		<h1 className="past-events">Past Events</h1>
+
+
+					 		<section className="section-events">
+						 		<div className="events-info-body">
+
+						 			<h1>Author Reading reading and Book Signing</h1>
+
+						 			<div className="events-wrapper">
+
+							 			<span className="events-title">Date</span>
+							 			<p>Wednesday, February 27, 2019</p>
+
+							 			<span className="events-title">Place</span>
+							 			<p> NYU Bookstore 726 Broadway</p>
+							 			<p>New York, 10003</p>
+
+							 			<span className="events-title">Time</span>
+							 			<p>6:00pm</p>
+
+							 			<p>*Event subject to change</p>
+
+						 			</div>
+						 		</div>
+
+						 		<div className="events-info-body">
+						 			<h1>White Plains Comicfest</h1>
+
+						 			<div className="events-wrapper">
+							 			
+							 			<span className="events-title">Date</span>
+							 			<p>Saturday July 20, 2019</p>
+							 			
+							 			<span className="events-title">Place</span>
+							 			<p>West 135 Street & Malcolm X Boulevard</p>
+
+							 			<span className="events-title">Time</span>
+							 			<p>9:00am to 6:00pm (expected, not verified)</p>
+							 			<p>*Event subject to change</p>
+
+						 			</div>
+						 		</div>
+
+						 		<div className="events-info-body">
+						 			<h1>Bronx Book Fair</h1>
+
+						 			<div className="events-wrapper">
+							 			
+							 			<span className="events-title">Date</span>
+							 			<p>Saturday May 4, 2019</p>
+							 			
+							 			<span className="events-title">Place</span>
+							 			<p>Bronx Library Center/New York Public Library</p>
+							 			<p>310 East KingBridge Road</p>
+							 			<p>Bronx, New York, 10458</p>
+
+							 			<span className="events-title">Time</span>
+							 			<p>11:00am to 7:00pm</p>
+							 			<p>*Event subject to change</p>
+
+						 			</div>
+						 		</div>
+
+					 	</section>
+
+					</div>
+				</div>
+
 		 	</Layout>
 		</>
 	)
